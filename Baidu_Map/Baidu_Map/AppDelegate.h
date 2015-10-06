d1ee166ad5d7a9e156df6b1a47aa1ddc605429ff
lanou3g <312731528@qq.com>
2015-10-06 14:50:27 +0800

    test

---
+++ Baidu_Map/Baidu_Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baidu_Map
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 王靖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
