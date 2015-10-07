19143a8f63caf072a08b4bceb47162e38e7b0b7b
lanou3g <312731528@qq.com>
2015-10-07 09:52:31 +0800

    ajkdhskasgdjkasgdkjashkdj

--- Baidu_Map/Baidu_Map/AppDelegate.m
+++ Baidu_Map/Baidu_Map/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,7 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    
+    ViewController *vc = [ViewController new];
+    
+    self.window.rootViewController = vc;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Baidu_Map/Baidu_Map/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Baidu_Map
+//
+//  Created by lanou3g on 15/10/7.
+//  Copyright (c) 2015年 王靖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
